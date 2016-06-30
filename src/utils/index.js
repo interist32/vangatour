55dfecf21e65ae280fd830b113ab7acc25dc212f
Vladimir Makushev <interist32@gmail.com>
2016-06-30 20:37:38 +0300

    Stupid bug

--- src/utils/index.js
+++ src/utils/index.js
@@ -5,6 +5,7 @@ export function getSiteLabels(){
   switch(location.host.toLowerCase()){
     case "vangatour.ru":
       retVal['siteName'] = 'Vanga Tour';
+      break;
     default:
       retVal['siteName'] = 'Ask2Go';
   }
